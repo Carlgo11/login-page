576af5c283c73b409a699a6f707ab13223590127
Carlgo11 <Carlgo11@users.noreply.github.com>
2018-07-15 06:25:07 +0200

    Add login system test

---
+++ login.php
@@ -0,0 +1,3 @@
+<?php
+if($_POST['username'] == 'test' && $_POST['password'] == 'test'){echo "true";}else{echo 'false';}
+?>
--- main.js
+++ main.js
@@ -1,24 +1,58 @@
 $(document).ready(function(e) {
-  $('#login-button').on("click", function() {
-    invalidLogin();
+  $('#login-button').click(function() {
+    sendLogin();
+    return false;
+  });
+
+  $('#login-body').submit(function() {
+    sendLogin();
+    return false;
+  });
+  /**
+   *
+   *
+   */
+  function loadNewPage() {
     /*
-    loadNewPage();
+    setInterval(function() {
+       $('#login-button').animate({"padding": '+=3'}, 'slow');
+       $('#login-button').animate({"padding": '-=3'}, 'slow');
+    }, 1000);
+     */
     $('#login-head').slideUp('slow');
     $('#login-head').promise().done(function() {
       $('.input-div').fadeOut();
     });
-    */
-  });
-  function loadNewPage() {
-   setInterval(function() {
-      $('#login-button').animate({"padding": '+=3'}, 'slow');
-      $('#login-button').animate({"padding": '-=3'}, 'slow');
-    }, 1000);
   }
 
-  function invalidLogin(){
-    $('.center').animate({"left": '-=70'}, 100);
-    $('.center').animate({"left": '+=140'}, 100);
-    $('.center').animate({"left": '-=70'}, 100);
+  function invalidLogin() {
+    $('.center').animate({
+      "left": '-=70'
+    }, 100);
+    $('.center').animate({
+      "left": '+=140'
+    }, 100);
+    $('.center').animate({
+      "left": '-=70'
+    }, 100);
+  }
+
+  function sendLogin() {
+    $.ajax({
+      type: "POST",
+      url: 'login.php',
+      data: {
+        username: $("#username").val(),
+        password: $("#password").val()
+      },
+      success: function(data) {
+        if (data === 'true') {
+          loadNewPage(); //valid login.
+        } else {
+          invalidLogin(); //invalid login.
+        }
+      }
+    });
+
   }
 });
